9df9282b5139de010ffc71a1864744c728ddab64
Matt Rocco <roccoma504@gmail.com>
2015-11-16 21:25:44 -0500

    Comment updates
    Updated code comments.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,30 +11,32 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     
-
-    
     // Define an audio player.
     var audioPlayer:AVAudioPlayer!
     var audioPlayerEcho:AVAudioPlayer!
     
-    // Define a variable to catch the pushed audio from the record view controller.
+    // Defines a variable to catch the pushed audio from the record view controller.
     var receivedAudio:RecordedAudio!
     
-    // Define an instance of audio engine.
+    // Defines an instance of audio engine.
     var audioEngine:AVAudioEngine!
     
-    // Define an instance of audio file.
+    // Defines an instance of audio file.
     var audioFile:AVAudioFile!
     
-    // Define an instance of an audio player node. This is global to allow external stopping.
+    // Defines an instance of an audio player node. This is global to allow external stopping.
     let audioPlayerNode = AVAudioPlayerNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Define an auto player with the file path that was passed in from
+        // the first view.
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, fileTypeHint: ".wav")
         audioPlayer.enableRate = true
         
+        // Define an auto player with the file path that was passed in from
+        // the first view. This player is used in the echo function.
         audioPlayerEcho = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, fileTypeHint: ".wav")
         audioPlayerEcho.enableRate = true
         
@@ -42,20 +44,24 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
     
+    // Defines a function that resets all audio.
     func resetAudio() {
-        
-        // Stop the player and the engine. Reset the engine.
+        // Stop the player, node and the engine. Reset the engine.
         audioPlayer.stop()
         audioPlayerNode.stop()
         audioEngine.stop()
         audioEngine.reset()
     }
     
+    //# MARK: - Audio modulation functions
+    
+    // Defines a function that will play the audio with the input
+    // pitch and rate.
     func playAudioWithEffect (Pitch : Float, Rate : Float) {
-        
         // Define an instance of unit time pitch for the desired rate effect.
         let changeEffect = AVAudioUnitTimePitch()
         
+        // Reset the audio player.
         resetAudio()
         
         // Define audio engine locally.
@@ -79,13 +85,14 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
         // Play the audio file.
         audioPlayerNode.play()
-        
     }
     
+    // Defines a function that will play the audio with reverb.
     func playAudioWithReverb (Reverb : Float) {
-        
+        // Defines the change effect instance.
         let changeEffect = AVAudioUnitReverb ()
         
+        // Reset the audio player.
         resetAudio ()
         
         // Define audio engine locally.
@@ -108,27 +115,34 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
         
         // Play the audio file.
         audioPlayerNode.play()
-        
     }
     
+    // Defines a function that will play the audio with echo.
     func playAudioWithEcho (Delay : Double) {
-        
+        // Reset the audio player.
         resetAudio ()
 
         // Stop the player and the engine. Reset the engine.
         audioPlayerEcho.stop()
-        audioPlayerEcho.currentTime = 0;
+        audioPlayerEcho.currentTime = 0
         audioPlayer.play()
         
         let delay:NSTimeInterval = Delay //ms
         var playtime:NSTimeInterval
-        playtime = audioPlayerEcho.deviceCurrentTime + delay
-        audioPlayerEcho.currentTime = 0
-        audioPlayerEcho.volume = 1.0;
-        audioPlayerEcho.playAtTime(playtime)
         
+        // Define the playtime based on the current time and the 
+        // input delay.
+        playtime = audioPlayerEcho.deviceCurrentTime + delay
+        
+        // Set the current time to 0 and the volume to max.
+        audioPlayerEcho.currentTime = 0
+        audioPlayerEcho.volume = 1.0
+        
+        // Schedule the audio playing.
+        audioPlayerEcho.playAtTime(playtime)
     }
     
+    //# MARK: - All IB object functions
     
     @IBAction func stopButtonPress(sender: AnyObject) {
         // Stop the audio player early.
@@ -164,4 +178,3 @@ class PlaySoundsViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
 }
-
--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -11,15 +11,18 @@ import AVFoundation
 
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
-    
+    // Define all outlets
     @IBOutlet weak var recordLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     
+    // Define an audio recorder. This will contain the recorded audio.
     var audioRecorder:AVAudioRecorder!
     
+    // Defines a function to default the UI. This is used on initialization
+    // and whenever the UI needs to be reset.
     func defaultUI() {
         recordLabel.text = "Press Microphone To Record"
         stopButton.hidden = true
@@ -35,12 +38,12 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
+    // Defines a function that is called when the audio is done recording.
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
-        //
+        // If the audio finished recording successfully, save off the audio.
+        // If not, log an error and default the UI.
         if (flag)
         {
             let recordedAudio = RecordedAudio ()
@@ -55,14 +58,15 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
-    @IBAction func startRecord(sender: AnyObject) {
-        
-        print("in record");
-        
+    //# MARK: - All IB object functions
+    
+    // Defines a function that is invoked when the record (microphone) button
+    // is pressed.
+    @IBAction func startButtonPressed(sender: AnyObject) {
         // Show the stop button.
         stopButton.hidden = false
         pauseButton.hidden = false
-
+        
         // Change the label to say recording.
         recordLabel.text = "Recording..."
         
@@ -85,31 +89,30 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
-        
     }
     
-    @IBAction func stopRecord(sender: AnyObject) {
-        
-        NSLog("in stop");
-        
+    // Defines a fuction that is invoked when the stop button is pressed.
+    @IBAction func stopButtonPressed(sender: AnyObject) {
         // Stop the audio recorder.
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
         
+        // Default the UI so it is back to ready when the user returns.
         defaultUI()
     }
     
-    @IBAction func pauseButtonPress(sender: AnyObject) {
+    // Defines a function that is invoked when the pause button is pressed.
+    @IBAction func pauseButtonPressed(sender: AnyObject) {
         // Hide the pause button. Show the play button.
         pauseButton.hidden = true;
         playButton.hidden = false;
         
         // Pause the recording.
         audioRecorder.pause()
-        
     }
     
+    // Defines a function that is invoked when the play button is pressed.
     @IBAction func playButtonPressed(sender: AnyObject) {
         // Show the pause button. Hide the play button.
         pauseButton.hidden = false;
@@ -119,15 +122,16 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.record()
     }
     
+    // Defines a function that is invoked when the stop button is pressed.
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // If the segue that is triggered is the "playSondSegue" then transistion
+        // to the next view controller and pass the recorded data.
         if (segue.identifier == "playSoundSegue")
         {
             let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
-            
             let data = sender as! RecordedAudio
             playSoundsVC.receivedAudio = data
         }
     }
     
 }
-
